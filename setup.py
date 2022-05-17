8d5110992f2130c1343a73bc3038e8e6574d9fc9
David Koop <dakoop@niu.edu>
2022-05-17 16:29:27 -0500

    Update for JupyterLab 3

--- setup.py
+++ setup.py
@@ -1,89 +1,95 @@
 """
-Setup Module to setup Python Package for the ipycollections extension.
+ipycollections setup
 """
-import os
+import json
+import sys
+from pathlib import Path
 
-from jupyter_packaging import (
-    create_cmdclass, install_npm, ensure_targets,
-    combine_commands, ensure_python, get_version,
-)
 import setuptools
 
-HERE = os.path.abspath(os.path.dirname(__file__))
+HERE = Path(__file__).parent.resolve()
+
+# Get the package info from package.json
+pkg_json = json.loads((HERE / "package.json").read_bytes())
 
 # The name of the project
-name="ipycollections"
+name = "ipycollections"
 
-# Ensure a valid python version
-ensure_python(">=3.5")
-
-# Get our version
-version = get_version(os.path.join(name, "_version.py"))
-
-lab_path = os.path.join(HERE, name, "labextension")
+lab_path = (HERE / pkg_json["jupyterlab"]["outputDir"])
 
 # Representative files that should exist after a successful build
-jstargets = [
-    os.path.join(HERE, "lib", "index.js"),
+ensured_targets = [
+    str(lab_path / "package.json"),
+    str(lab_path / "static/style.js")
 ]
 
-package_data_spec = {
-    name: [
-        "*"
-    ]
-}
+labext_name = pkg_json["name"]
 
 data_files_spec = [
-    ("share/jupyter/lab/extensions", lab_path, "*.tgz"),
-    # ("etc/jupyter/jupyter_notebook_config.d",
-    #  "jupyter-config", "ipycollections.json"),
+    ("share/jupyter/labextensions/%s" % labext_name, str(lab_path.relative_to(HERE)), "**"),
+    ("share/jupyter/labextensions/%s" % labext_name, str("."), "install.json"),
 ]
 
-cmdclass = create_cmdclass("jsdeps", 
-    package_data_spec=package_data_spec,
-    data_files_spec=data_files_spec
-)
+long_description = (HERE / "README.md").read_text()
 
-cmdclass["jsdeps"] = combine_commands(
-    install_npm(HERE, build_cmd="build:all", npm=["jlpm"]),
-    ensure_targets(jstargets),
-)
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+version = (
+    pkg_json["version"]
+    .replace("-alpha.", "a")
+    .replace("-beta.", "b")
+    .replace("-rc.", "rc")
+) 
 
 setup_args = dict(
     name=name,
     version=version,
-    url="https://github.com/dataflownb/ipycollections-extension",
-    download_url="https://github.com/dataflownb/ipycollections-extension/archive/v0.1.0.tar.gz",
-    author="David Koop",
-    description="A JupyterLab extension that provides a more interactive format for collections",
-    long_description= long_description,
+    url=pkg_json["homepage"],
+    author=pkg_json["author"]["name"],
+    author_email=pkg_json["author"]["email"],
+    description=pkg_json["description"],
+    license=pkg_json["license"],
+    license_file="LICENSE",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    cmdclass= cmdclass,
     packages=setuptools.find_packages(),
-    install_requires=[
-        "ipython>=6.0",
-        "jupyterlab~=2.0",
-    ],
+    install_requires=[],
     zip_safe=False,
     include_package_data=True,
-    license="BSD-3-Clause",
+    python_requires=">=3.7",
     platforms="Linux, Mac OS X, Windows",
-    keywords=["Jupyter", "JupyterLab", "IPython"],
+    keywords=["Jupyter", "JupyterLab", "JupyterLab3"],
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Framework :: Jupyter",
+        "Framework :: Jupyter :: JupyterLab",
+        "Framework :: Jupyter :: JupyterLab :: 3",
+        "Framework :: Jupyter :: JupyterLab :: Extensions",
+        "Framework :: Jupyter :: JupyterLab :: Extensions :: Prebuilt",
     ],
 )
 
+try:
+    from jupyter_packaging import (
+        wrap_installers,
+        npm_builder,
+        get_data_files
+    )
+    post_develop = npm_builder(
+        build_cmd="install:extension", source_dir="src", build_dir=lab_path
+    )
+    setup_args["cmdclass"] = wrap_installers(post_develop=post_develop, ensured_targets=ensured_targets)
+    setup_args["data_files"] = get_data_files(data_files_spec)
+except ImportError as e:
+    import logging
+    logging.basicConfig(format="%(levelname)s: %(message)s")
+    logging.warning("Build tool `jupyter-packaging` is missing. Install it with pip or conda.")
+    if not ("--name" in sys.argv or "--version" in sys.argv):
+        raise e
 
 if __name__ == "__main__":
     setuptools.setup(**setup_args)
